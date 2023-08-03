2dc4710a4e20f70141b625d62c76b87abc6d5e95
Dane Wilson <dnkw@hotmail.com>
2023-08-03 09:27:33 -0500

    Setup database models
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>

---
+++ db/migrate/20230803142156_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :description
+      t.datetime :due_date
+      t.datetime :completed_at
+      t.string :priority, default: Task.priorities[:low]
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/tasks.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :task do
+    user { nil }
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 0..4) }
+    due_date { 3.days.from_now  }
+    completed_at { nil }
+    priority { Task.priorities.values.sample }
+  end
+end
